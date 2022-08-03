2d038547b9263260d538df6dead9888759b3bc28
manishkumarmandane <bm0369@gmail.com>
2022-08-03 21:11:49 +0100

    table

---
+++ ForLooptableTen.java
@@ -0,0 +1,16 @@
+package Homework3_3;
+
+public class ForLooptableTen
+{
+    public static void main(String[] args) {
+        int num = 10, i, k;
+
+
+
+        for (i = 1; i <= 10; i++) {
+            k = num * i;
+
+            System.out.println(num + "*" + i + " = " + k);
+        }
+    }
+}
